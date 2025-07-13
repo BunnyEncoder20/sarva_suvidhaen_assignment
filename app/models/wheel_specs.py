a64bd6f8350aab3defd5b4d690ca9863da9e01c9
Varun Verma <varun.verma2024@gmail.com>
2025-07-13 19:02:38 +0530

    Create ORM models for wheel_spec table

---
+++ app/models/wheel_specs.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, String, Date, Integer
+from app.database import Base
+
+class WheelSpecification(Base):
+    __tablename__ = "wheel_specs"
+
+    # Outer fields
+    id = Column(Integer, primary_key=True, index=True)
+    form_number = Column(String, unique=True, nullable=False)
+    submitted_by = Column(String, nullable=False)
+    submitted_date = Column(Date, nullable=False)
+
+    # Fields inside 'fields' object
+    axle_box_housing_bore_dia = Column(String)
+    bearing_seat_diameter = Column(String)
+    condemning_dia = Column(String)
+    intermediate_wwp = Column(String)
+    last_shop_issue_size = Column(String)
+    roller_bearing_bore_dia = Column(String)
+    roller_bearing_outer_dia = Column(String)
+    roller_bearing_width = Column(String)
+    tread_diameter_new = Column(String)
+    variation_same_axle = Column(String)
+    variation_same_bogie = Column(String)
+    variation_same_coach = Column(String)
+    wheel_disc_width = Column(String)
+    wheel_gauge = Column(String)
+    wheel_profile = Column(String)
