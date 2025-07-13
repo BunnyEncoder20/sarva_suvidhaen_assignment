b9c5e51967df29d7a39e09117c258a1ef3c21ca6
Varun Verma <varun.verma2024@gmail.com>
2025-07-13 13:52:11 +0530

    basic project init

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # sarva_suvidhaen_assignment
+
+## Project Init
+
+1. setting up git repo
+```cmd
+echo "# sarva_suvidhaen_assignment" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/BunnyEncoder20/sarva_suvidhaen_assignment.git
+git push -u origin main
+```
+
+2. make a venv (I used uv) and actiavte it:
+```cmd
+uv venv
+source .venv/bin/activate
+```
+
+3. Init FastAPI project
+- Command installs:
+    1. fastapi
+    2. psycopg (Postgres Driver)
+    3. dotenv (for env variables)
+    4. sqlalchemy (ORM)
+```cmd
+uv pip install "fastapi[standard]" "psycopg[binary]" python-dotenv sqlalchemy
+```
+
+4. Made .env and config.py
+    - for loading all env variables from single src of truth - the config file. (also don't need to load all env in each and every file which needs it)
+
+5. Make a requirements.txt file
+```cmd
+uv pip freeze > requirements.txt
+```
---
+++ app/main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, status
+
+# routes import
+
+
+# database import
+# from app.database import Base, engine
+
+# CORS middleware
+from fastapi.middleware.cors import CORSMiddleware
+
+
+'''------------------------------------------------------------------'''
+
+
+# making FastAPI class instance
+app = FastAPI()
+
+# lists of alllowed origins that can reach out backend
+allowed_orgins = ['*']
+
+# Adding the CORS middleware to our app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_orgins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Commented out because Alembic took over to making PgSQL tables
+# Making connection to Postgres DB using SLQ Alchemy. reMakes the tables on app startup
+# Base.metadata.create_all(bind=engine)
+
+
+# Root route
+@app.get("/")
+async def root():
+    return {"status_code": status.HTTP_200_OK , "msg": "Hello Sarva Suvidhaen"}
+
+
+
+# include routers
